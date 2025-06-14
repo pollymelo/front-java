c1d5aea6b287f0cce9e8c464e2d24c3fc0fc5ea3
Gabriel Camara <gabriel25camr@gmail.com>
2025-06-14 09:36:37 -0300

    chore: Configura a estrutura inicial da tela do painel

--- src/components/Geral/Geral.java
+++ src/components/Geral/Geral.java
@@ -1,4 +1,25 @@
 package components.Geral;
 
-public class Geral {
+import javax.swing.*;
+import java.awt.*;
+
+public class Geral extends JFrame {
+
+    public Geral() {
+        initUI();
+    }
+
+    private void initUI() {
+        setTitle("Tela 2 - Geral");
+        setSize(900, 700);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            Geral ex = new Geral();
+            ex.setVisible(true);
+        });
+    }
 }
